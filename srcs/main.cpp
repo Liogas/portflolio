c0c20e2077d2a866cb1387b0e3c54b914401695e
Gaston Lions <glions@f4r14s2.clusters.42paris.fr>
2026-03-02 16:23:14 +0100

    exemple pour recuperer les resolutions dispo pour l'ecran

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 
+#include <iostream>
+
 int main(int argc, char** argv)
 {
     (void)argc;
@@ -21,16 +23,63 @@ int main(int argc, char** argv)
                                                                   640,
                                                                   480,
                                                                   SDL_WINDOW_SHOWN);
+                                                                  
+        int fullscreen = 0;
 
-        if( pWindow )
+        // On récupère le nombre de modes d'affichage pour l'écran 0 /////
+        int modeNumber = SDL_GetNumDisplayModes(0);
+        if (modeNumber < 0)
         {
-            SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voit la fenêtre */
-
-            SDL_DestroyWindow(pWindow);
+            fprintf(stdout,"Échec lors de la récupération du nombre de modes (%s)\n",SDL_GetError());
+            return -2;
         }
-        else
+        fprintf(stdout,"Vous avez %d mode(s) d'affichage\n", modeNumber);
+
+        // Affichage des différents modes
+        SDL_DisplayMode displayMode;
+        int error;
+        for (int i = 0 ; i < modeNumber ; i++)
         {
-            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
+            error = SDL_GetDisplayMode(0, i, &displayMode);
+            if (error < 0)
+            {
+                fprintf(stdout, "Échec lors de la récupération du mode d'affichage (%s)\n", SDL_GetError());
+                return -3;
+            }
+            
+            fprintf(stdout, "Mode %d : %dx%dx%d\n", i, displayMode.w, displayMode.h, displayMode.refresh_rate);
+        }
+        ////////////////////////////////////////////////////////////////////////
+        while ( pWindow )
+        {
+            SDL_Event event;
+            while (SDL_PollEvent(&event)) // Récupération des actions de l'utilisateur
+            {
+                switch(event.type)
+                {
+                    case SDL_QUIT: // Clic sur la croix
+                        // quit=1;
+                        SDL_DestroyWindow(pWindow);
+                        pWindow = NULL;
+                        break;
+                    case SDL_KEYUP: // Relâchement d'une touche
+                        if ( event.key.keysym.sym == SDLK_f ) // Touche f
+                        {
+                            // Alterne du mode plein écran au mode fenêtré
+                            if ( fullscreen == 0 )
+                            {
+                                fullscreen = 1;
+                                SDL_SetWindowFullscreen(pWindow,SDL_WINDOW_FULLSCREEN);
+                            }
+                            else if ( fullscreen == 1 )
+                            {
+                                fullscreen = 0;
+                                SDL_SetWindowFullscreen(pWindow,0);
+                            }
+                        }
+                        break;
+                }
+            }
         }
     }
 
