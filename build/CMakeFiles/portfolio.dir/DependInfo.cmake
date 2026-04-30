165976f109fdf7d0bf7ba2ced85ed41c25aa3b79
Nostag <gastonlions.pro@gmail.com>
2026-04-30 14:21:18 +0200

    ajout du CMake mais revoir le code les images ne chargent plus

---
+++ build/CMakeFiles/portfolio.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/glions/portflolio/MySDL/srcs/AnimationSDL.cpp" "CMakeFiles/portfolio.dir/MySDL/srcs/AnimationSDL.cpp.o" "gcc" "CMakeFiles/portfolio.dir/MySDL/srcs/AnimationSDL.cpp.o.d"
+  "/home/glions/portflolio/MySDL/srcs/AppSDL.cpp" "CMakeFiles/portfolio.dir/MySDL/srcs/AppSDL.cpp.o" "gcc" "CMakeFiles/portfolio.dir/MySDL/srcs/AppSDL.cpp.o.d"
+  "/home/glions/portflolio/MySDL/srcs/EventSDL.cpp" "CMakeFiles/portfolio.dir/MySDL/srcs/EventSDL.cpp.o" "gcc" "CMakeFiles/portfolio.dir/MySDL/srcs/EventSDL.cpp.o.d"
+  "/home/glions/portflolio/MySDL/srcs/InputSDL.cpp" "CMakeFiles/portfolio.dir/MySDL/srcs/InputSDL.cpp.o" "gcc" "CMakeFiles/portfolio.dir/MySDL/srcs/InputSDL.cpp.o.d"
+  "/home/glions/portflolio/MySDL/srcs/RendererSDL.cpp" "CMakeFiles/portfolio.dir/MySDL/srcs/RendererSDL.cpp.o" "gcc" "CMakeFiles/portfolio.dir/MySDL/srcs/RendererSDL.cpp.o.d"
+  "/home/glions/portflolio/MySDL/srcs/TextureSDL.cpp" "CMakeFiles/portfolio.dir/MySDL/srcs/TextureSDL.cpp.o" "gcc" "CMakeFiles/portfolio.dir/MySDL/srcs/TextureSDL.cpp.o.d"
+  "/home/glions/portflolio/MySDL/srcs/WindowSDL.cpp" "CMakeFiles/portfolio.dir/MySDL/srcs/WindowSDL.cpp.o" "gcc" "CMakeFiles/portfolio.dir/MySDL/srcs/WindowSDL.cpp.o.d"
+  "/home/glions/portflolio/srcs/core/App.cpp" "CMakeFiles/portfolio.dir/srcs/core/App.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/core/App.cpp.o.d"
+  "/home/glions/portflolio/srcs/core/RessourceManager.cpp" "CMakeFiles/portfolio.dir/srcs/core/RessourceManager.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/core/RessourceManager.cpp.o.d"
+  "/home/glions/portflolio/srcs/core/SceneManager.cpp" "CMakeFiles/portfolio.dir/srcs/core/SceneManager.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/core/SceneManager.cpp.o.d"
+  "/home/glions/portflolio/srcs/core/main.cpp" "CMakeFiles/portfolio.dir/srcs/core/main.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/core/main.cpp.o.d"
+  "/home/glions/portflolio/srcs/core/scene/GameScene.cpp" "CMakeFiles/portfolio.dir/srcs/core/scene/GameScene.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/core/scene/GameScene.cpp.o.d"
+  "/home/glions/portflolio/srcs/core/tinyxml2.cpp" "CMakeFiles/portfolio.dir/srcs/core/tinyxml2.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/core/tinyxml2.cpp.o.d"
+  "/home/glions/portflolio/srcs/game/Computer.cpp" "CMakeFiles/portfolio.dir/srcs/game/Computer.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/game/Computer.cpp.o.d"
+  "/home/glions/portflolio/srcs/game/GameState.cpp" "CMakeFiles/portfolio.dir/srcs/game/GameState.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/game/GameState.cpp.o.d"
+  "/home/glions/portflolio/srcs/game/Player.cpp" "CMakeFiles/portfolio.dir/srcs/game/Player.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/game/Player.cpp.o.d"
+  "/home/glions/portflolio/srcs/game/TileMap.cpp" "CMakeFiles/portfolio.dir/srcs/game/TileMap.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/game/TileMap.cpp.o.d"
+  "/home/glions/portflolio/srcs/graphics/Camera.cpp" "CMakeFiles/portfolio.dir/srcs/graphics/Camera.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/graphics/Camera.cpp.o.d"
+  "/home/glions/portflolio/srcs/graphics/ComputerRenderer.cpp" "CMakeFiles/portfolio.dir/srcs/graphics/ComputerRenderer.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/graphics/ComputerRenderer.cpp.o.d"
+  "/home/glions/portflolio/srcs/graphics/MapRenderer.cpp" "CMakeFiles/portfolio.dir/srcs/graphics/MapRenderer.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/graphics/MapRenderer.cpp.o.d"
+  "/home/glions/portflolio/srcs/graphics/Sprite.cpp" "CMakeFiles/portfolio.dir/srcs/graphics/Sprite.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/graphics/Sprite.cpp.o.d"
+  "/home/glions/portflolio/srcs/parseur/MapParseur.cpp" "CMakeFiles/portfolio.dir/srcs/parseur/MapParseur.cpp.o" "gcc" "CMakeFiles/portfolio.dir/srcs/parseur/MapParseur.cpp.o.d"
+  "" "portfolio" "gcc" "CMakeFiles/portfolio.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
